88ab12f07c0328d35fb8bb11b2d273c58a81a018
matdzone <mkucas04@gmail.com>
2026-03-12 20:30:51 +0200

    Pakeitimai

---
+++ sandeliukas/src/main/java/wms/sandeliukas/SandeliukasApplication.java
@@ -0,0 +1,13 @@
+package wms.sandeliukas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SandeliukasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SandeliukasApplication.class, args);
+	}
+
+}
---
+++ sandeliukas/src/test/java/wms/sandeliukas/SandeliukasApplicationTests.java
@@ -0,0 +1,13 @@
+package wms.sandeliukas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SandeliukasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
